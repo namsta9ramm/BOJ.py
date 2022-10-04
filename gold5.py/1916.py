4e15ba5c51c57d42118e8724cb4601ba3a298199
namkyounghyun <david1510@naver.com>
2022-10-04 11:24:30 +0900

    solve algorithm

--- gold5.py/1916.py
+++ gold5.py/1916.py
@@ -1,55 +1,28 @@
-#다익스트라 알고리즘
-#첫째줄에 정점의 개수 V와 간선의 개수 E가 주어집니다.
-#두번째 줄에는 시작정점의 번호 k가 주어진다.
-#세번째 줄부터 E개의 줄을 거쳐 간선을 나타내는 세 정수가 나타난다.
-
 import sys
+from heapq import heappush, heappop
 input = sys.stdin.readline
-INF = int(1e9)
+n = int(input())
+m = int(input())
 
-n=int(input())
-m=int(input())
-#시작노드
-# 주어지는 그래프 정보 담는 N개 길이의 리스트
-graph = [[] for _ in range(n+1)]
-visited = [False] * (n+1)  # 방문처리 기록용
-distance = [INF] * (n+1)   # 거리 테이블용
-
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-start,end= map(int,input().split())
-# 방문하지 않은 노드이면서 시작노드와 최단거리인 노드 반환한다
-def get_smallest_node():
-    min_value = INF
-    M=0
-    index = 0
-    for i in range(1, n+1):
-        if not visited[i] and distance[i] < min_value:
-            min_value = distance[i]
-            index = i
-    return index
-
-# 다익스트라 알고리즘
+inf = 100000000
+s = [[] for i in range(n + 1)]
+dp = [inf for i in range(n + 1)]
+for i in range(m):
+    a, b, w = map(int, input().split())
+    s[a].append([b, w])
+start, end = map(int, input().split())
 def dijkstra(start):
-    # 시작노드 -> 시작노드 거리 계산 및 방문처리
-    distance[start] = 0
-    visited[start] = True
-    # 시작노드의 인접한 노드들에 대해 최단거리 계산
-    for i in graph[start]:
-        distance[i[0]] = i[1]
-
-    # 시작노드 제외한 n-1개의 다른 노드들 처리
-    for _ in range(n-1):
-        now = get_smallest_node()  # 방문X 면서 시작노드와 최단거리인 노드 반환
-        visited[now] = True        # 해당 노드 방문처리
-        # 해당 노드의 인접한 노드들 간의 거리 계산
-        for next in graph[now]:
-            cost = distance[now] + next[1]  # 시작->now 거리 + now->now의 인접노드 거리
-            if cost < distance[next[0]]:    # cost < 시작->now의 인접노드 다이렉트 거리
-                distance[next[0]] = cost
-
-
+    dp[start] = 0
+    heap = []
+    heappush(heap, [0, start])
+    while heap:
+        w, n = heappop(heap)
+        if dp[n] < w:
+            continue
+        for n_n, wei in s[n]:
+            n_w = w + wei
+            if dp[n_n] > n_w:
+                dp[n_n] = n_w
+                heappush(heap, [n_w, n_n])
 dijkstra(start)
-
-print(distance[end])
+print(dp[end])
---
+++ newone.py/25501.py
@@ -0,0 +1,19 @@
+count=0
+def recursion(s, l, r):
+    global count
+    count=count+1
+    if l >= r: 
+        return 1
+    elif s[l] != s[r]: 
+        return 0
+    else:       
+        return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+a=int(input())
+for i in range(a):
+    count=0
+    a=input()
+    print(isPalindrome(a),count)
