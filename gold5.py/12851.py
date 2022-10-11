5d45dd24a456a410c36c3e205668e8a54cc0009e
namkyounghyun <david1510@naver.com>
2022-10-11 16:12:28 +0900

    solve algorithm

---
+++ gold5.py/12851.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n, k = map(int, input().split())
+queue = deque()
+queue.append(n)
+visited = [0] * 100001
+visited[n] = 0
+
+ans_count = 0
+ans_way = 0
+while queue:
+    x = queue.popleft()
+    if x == k:
+        ans_count = visited[x]
+        ans_way += 1
+        
+        continue
+        
+    for nx in [x-1, x+1, 2*x]:
+        if 0 <= nx < 100001:
+            if visited[nx] == 0 or visited[nx] == visited[x] + 1:
+                queue.append(nx)
+                visited[nx] = visited[x] + 1
+        print(queue)
+
+print(ans_count)
+print(ans_way)
