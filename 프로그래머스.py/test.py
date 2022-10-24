a3309960bf5643c905c2d91edf9d978182376f5e
namkyounghyun <david1510@naver.com>
2022-10-24 21:37:21 +0900

    solve algorithm

--- 프로그래머스.py/test.py
+++ 프로그래머스.py/test.py
@@ -1,5 +1,23 @@
-new_list=[[1,2,3],[4,5,6],[7,8,9],['*',0,'#']]
-for i in new_list:
-    if 4 in i:
-        print(new_list.index(i),i.index(4))
+def n_queen(n, x, queen):
+    result = 0
+    if x == n:
+        return 1
+    for i in range(n):
+        queen[x] = i
+        
+        if check(x, queen):
+            result += n_queen(n, x+1, queen)
+
+    return result
+
+def check(x, queen):
+    for i in range(x):
+        if queen[x] == queen[i] or abs(x-i) == abs(queen[x] - queen[i]):
+            return False
+    return True
     
+def solution(n):
+    queen = [0] * n
+    answer = n_queen(n, 0, queen)
+    
+    return answer
